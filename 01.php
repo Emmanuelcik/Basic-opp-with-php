9169e32a3234c5c43759c120fa9c618310a39424
Emmanuel López <jesusemm010@hotmail.com>
2021-05-17 22:51:33 -0500

    how to create a class and instance objects

--- 01.php
+++ 01.php
@@ -1,4 +1,28 @@
 <?php include 'includes/header.php';
 
+    //Define a class with the reserved word class
+    class Product {
+        //define the attributes
+        public $name;
+        public $price;
+        public $disponibility;
+
+    }
+    //create a new instance
+    $product = new Product();
+    $product->name = "Tablet";
+    $product->price = 2000;
+    $product->disponibility = true;
+    echo "<pre>";
+    var_dump($product);
+    echo "</pre>";
+    //Create a new instance of another product
+    $product2 = new Product();
+    $product2->name = "Laptop";
+    $product2->price = 4000;
+    $product2->disponibility = false;
+    echo "<pre>";
+    var_dump($product2);
+    echo "</pre>";
 
 include 'includes/footer.php';
--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
-</div>
-</div>
+</div> <!--DIV de resultado-->
+</div>  <!-- Div final del .contenido -->
 
 </body>
 
