1e125e91d91c270a621a21a4b8c58c8fd4e41ea8
Emmanuel López <jesusemm010@hotmail.com>
2021-05-19 23:51:37 -0500

    inheritance

--- 04.php
+++ 04.php
@@ -1,4 +1,46 @@
 <?php include 'includes/header.php';
 
+    class Transport {
+
+        public function __construct(protected int $wheels, protected int $capacity)
+        { 
+
+        }
+
+        public function getInfo() : string{
+            return "This trasnport has " . $this->wheels . " wheels and a capacity of " . $this->capacity. " persons"; 
+        }
+        public function getWheels(){
+            return $this->wheels;
+        }
+    }
+
+    class Bicycle extends Transport{
+        public function getInfo() :string {
+            return "This trasnport has " . $this->wheels . " wheels and a capacity of " . $this->capacity. " persons and it is sustainable "; 
+        }
+    }
+    class Car extends Transport{
+        
+        public function __construct(protected int $wheels, protected int $capacity, protected string $transmission)
+        { 
+
+        }
+        public function getTrans(): string{
+            return $this->transmission; 
+        }
+    }
+
+    $bike = new Bicycle(2,1);
+    echo $bike->getInfo();
+    echo $bike->getWheels();
+    echo "<hr>";
+    $car = new Car(4,100, "automatic");
+    echo $car->getInfo();
+    echo "<br>";
+    echo $car->getTrans();
+    echo " "; 
+    echo $car->getWheels();
+    echo "<hr>";
 
 include 'includes/footer.php';
