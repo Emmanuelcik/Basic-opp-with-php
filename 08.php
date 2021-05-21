2ce72ef6179f0b3b577d625fb9d4dae7d1e4fdce
Emmanuel López <jesusemm010@hotmail.com>
2021-05-20 23:38:59 -0500

    Autoload in PHP

--- 08.php
+++ 08.php
@@ -1,4 +1,13 @@
 <?php include 'includes/header.php';
 
+function my_autoload($class){
+    echo $class;
+    require __DIR__ . "/classes/".  $class . ".php"; 
+}
+spl_autoload_register("my_autoload");
+
+$details = new Details();
+echo "<br>";
+$clients = new Client();
 
 include 'includes/footer.php';
