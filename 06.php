0196ed4f38ad86da5b0130f6444b1f531cff15ad
Emmanuel López <jesusemm010@hotmail.com>
2021-05-20 23:06:55 -0500

    Interfaces

--- 06.php
+++ 06.php
@@ -1,4 +1,27 @@
-<?php include 'includes/header.php';
+<?php
 
+use Transport as GlobalTransport;
+
+include 'includes/header.php';
+
+interface interfaceTransport{
+    public function getInfo() : string;
+    public function getWheels() : int;
+}
+
+abstract class Transport {
+
+    public function __construct(protected int $wheels, protected int $capacity)
+    { 
+
+    }
+
+    public function getInfo() : string{
+        return "This trasnport has " . $this->wheels . " wheels and a capacity of " . $this->capacity. " persons"; 
+    }
+    public function getWheels() : int{
+        return $this->wheels;
+    }
+}
 
 include 'includes/footer.php';
